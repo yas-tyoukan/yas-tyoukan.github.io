114213437a76a163d3cade0e772ba7f14de461ae
yas-tyoukan <uuaauo@gmail.com>
2016-01-17 22:39:28 +0900

    エディター実装追加

--- y-github-io/maze/maze.js
+++ y-github-io/maze/maze.js
@@ -1,7 +1,6 @@
 (function() {
 	// 定数定義
 
-
 	/**
 	 * 迷路クラス
 	 *
@@ -10,8 +9,10 @@
 	function Maze(mazeData, rowNum, colNum) {
 		this._data = mazeData;
 	}
+
+
 	Maze.prototype = {
-		getMazeData: function() {
+		getMazeData : function() {
 			return this._data;
 		}
 	};
@@ -25,38 +26,40 @@
 		this._root = [];
 		this._maze = maze
 	}
+
+
 	Root.prototype = {
-		back: function() {},
-		getNextRoot: function() {
+		back : function() {
+		},
+		getNextRoot : function() {
 
 		}
 	};
 
-
 	h5.u.obj.expose('app.maze', {
-		Maze: Maze
+		Maze : Maze
 	});
 })();
 (function() {
 	var controller = {
-		__name: 'app.maze.MazeLogic',
-		_maze: null,
-		_currentEditor: null,
-		__construct: function() {
+		__name : 'app.maze.MazeLogic',
+		_maze : null,
+		_currentEditor : null,
+		__construct : function() {
 			// TODO デフォルト迷路の作成
-			var mazeData = [['s', ' ', ' ', ' '], [' ', ' ', '#', ' '], [' ', ' ', 'g', ' ']];
+			var mazeData = ['######', '#s   #', '#  # #', '#  g #', '######'];
 			this._maze = new app.maze.Maze(mazeData, 3, 4);
 		},
-		getMaze: function() {
+		getMaze : function() {
 			return this._maze;
 		},
-		setMaze: function(maze) {
+		setMaze : function(maze) {
 			this._maze = maze;
 		},
-		getShortestRoot: function() {
+		getShortestRoot : function() {
 
 		},
-		getShortestRoot: function() {
+		getShortestRoot : function() {
 
 		}
 	};
@@ -64,62 +67,88 @@
 })();
 
 (function() {
+	var dataToView = {
+		' ' : '□',
+		'#' : '■',
+		's' : 's',
+		'g' : 'g'
+	};
 	var controller = {
-		__name: 'app.maze.CUIEditorController',
-		_maze: null,
-		__init: function() {
-			this._$textArea = $('<textarea>');
-			$(this.rootElement).append(this._$textArea);
-		},
-		__ready: function() {
-			if (!this._maze) {
-				return;
+		__name : 'app.maze.ViewController',
+		setMaze : function(maze) {
+			var mazeData = maze.getMazeData();
+			var $rootElement = $(this.rootElement);
+			$rootElement.empty();
+			for (var i = 0, l = mazeData.length; i < l; i++) {
+				var row = mazeData[i];
+				for (var j = 0, len = row.length; j < len; j++) {
+					var d = row[j];
+					$rootElement.append('<span class="square">' + dataToView[d] + '</span>');
+				}
+				$rootElement.append('<br>');
 			}
+		}
+	};
+	h5.core.expose(controller);
+})();
+
+(function() {
+	var controller = {
+		__name : 'app.maze.CUIEditorController',
+		_maze : null,
+		__init : function() {
+			this._$textArea = this.$find('textarea');
+		},
+		createMaze : function() {
+			var str = this._$textArea.val();
+			var mazeData = [];
+			var rows = str.split('\n');
+			var rowNum = rows.length;
+			var colNum = 0;
+			for (var i = 0; i < rowNum; i++) {
+				var r = rows[i];
+				var l = r.length;
+				mazeData.push(r);
+				colNum = colNum < l ? l : colNum;
+			}
+			return new app.maze.Maze(mazeData, rowNum, colNum);
+		},
+		setMaze : function(maze) {
+			this._maze = maze;
 			var mazeData = this._maze.getMazeData();
 			var str = '';
 			for (var i = 0, l = mazeData.length; i < l; i++) {
-				str += mazeData[i].join('') + '\n';
+				str += mazeData[i] + '\n';
 			}
 			this._$textArea.val(str);
-		},
-		createMaze: function() {
-
-		},
-		setMaze: function(maze) {
-			this._maze = maze;
-		},
-		hide: function() {
-			$(this.rootElement).addClass('hidden');
-		},
-		show: function() {
-			$(this.rootElement).removeClass('hidden');
 		}
 	};
 	h5.core.expose(controller);
 })();
 (function() {
 	var controller = {
-		__name: 'app.maze.GUIEditorController',
-		_maze: null,
-		__init: function() {
-		},
-		__ready: function() {
-			if (!this._maze) {
-				return;
+		__name : 'app.maze.GUIEditorController',
+		mazeViewController : app.maze.ViewController,
+		__meta : {
+			mazeViewController : {
+				rootElement : null
 			}
-			var mazeData = this._maze.getMazeData();
 		},
-		createMaze: function() {
+		_maze : null,
+		__init : function() {
+			this.$mazeView = this.$find('.mazeView');
+			this.__meta.mazeViewController.rootElement = this.$mazeView;
+		},
+		createMaze : function() {
+			var mazeData = [];
+			this.$mazeView.children().each(function() {
 
+			});
+			return new app.maze.Maze(mazeData);
 		},
-		setMaze: function(maze) {
+		setMaze : function(maze) {
 			this._maze = maze;
-		},
-		hide: function() {
-			$(this.rootElement).addClass('hidden');
-		},
-		show: function() {
-			$(this.rootElement).removeClass('hidden');
+			this.mazeViewController.setMaze(maze);
 		}
 	};
 	h5.core.expose(controller);
@@ -127,34 +156,32 @@
 
 (function() {
 	var controller = {
-		__name: 'app.maze.PageController',
-		mazeLogic: app.maze.MazeLogic,
-		cuiEditorController: app.maze.CUIEditorController,
-		guiEditorController: app.maze.GUIEditorController,
-		__meta: {
-			cuiEditorController: {
-				rootElement: '.CUIEditor'
+		__name : 'app.maze.PageController',
+		mazeLogic : app.maze.MazeLogic,
+		cuiEditorController : app.maze.CUIEditorController,
+		guiEditorController : app.maze.GUIEditorController,
+		__meta : {
+			cuiEditorController : {
+				rootElement : '.CUIEditor'
 			},
-			guiEditorController: {
-				rootElement: '.GUIEditor'
+			guiEditorController : {
+				rootElement : '.GUIEditor'
 			}
 		},
-		__ready: function() {
+		__ready : function() {
 			var cuiEditor = this.cuiEditorController;
 			var guiEditor = this.guiEditorController;
 			var maze = this.mazeLogic.getMaze();
-			guiEditor.hide();
-			cuiEditor.show();
 			cuiEditor.setMaze(maze);
-			this._currentEditor = cuiEditor;
+			guiEditor.setMaze(maze);
+			this._currentEditor = $(cuiEditor.rootElement).hasClass('hidden') ? guiEditor : cuiEditor;
 		},
-		'.changeEditor click': function(ctx, $el) {
+		'.changeEditor click' : function(ctx, $el) {
 			var maze = this._currentEditor.createMaze();
-			var targetEditor = this._currentEditor === this.cuiEditorController ? this.guiEditorController
-					: this.cuiEditorController;
+			var targetEditor = this._currentEditor === this.cuiEditorController ? this.guiEditorController : this.cuiEditorController;
 			targetEditor.setMaze(maze);
-			this._currentEditor.hide();
-			targetEditor.show();
+			$(targetEditor.rootElement).removeClass('hidden');
+			$(this._currentEditor.rootElement).addClass('hidden');
 			this._currentEditor = targetEditor;
 
 		}
